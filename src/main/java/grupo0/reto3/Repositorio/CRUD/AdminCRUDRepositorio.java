936ea2057282620bd365f52b32bebdadbaa7232b
Pedro Leal <pedro.leal.mt@usa.edu.co>
2022-10-08 13:55:15 -0400

    Primer Commit

--- src/main/java/grupo0/reto3/Repositorio/CRUD/AdminCRUDRepositorio.java
+++
@@ -1,9 +0,0 @@
-package grupo0.reto3.Repositorio.CRUD;
-
-
-import grupo0.reto3.Modelo.Admin;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AdminCRUDRepositorio extends CrudRepository<Admin,Integer>
-{
-}
